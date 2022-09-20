89be04d1851431f6082b04072825e92b7ac8ca87
martialtimira <zignar1999@gmail.com>
2022-09-20 19:07:48 +0200

    Initial commit

---
+++ src/aichess.py
@@ -0,0 +1,193 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  8 11:22:03 2022
+
+@author: ignasi
+"""
+
+import chess
+import numpy as np
+import sys
+
+from itertools import permutations
+
+
+class Aichess():
+
+    """
+    A class to represent the game of chess.
+
+    ...
+
+    Attributes:
+    -----------
+    chess : Chess
+        represents the chess game
+
+    Methods:
+    --------
+    startGame(pos:stup) -> None
+        Promotes a pawn that has reached the other side to another, or the same, piece
+
+    """
+
+    def __init__(self, TA, myinit=True):
+
+        if myinit:
+            self.chess = chess.Chess(TA, True)
+        else:
+            self.chess = chess.Chess([], False)
+
+        self.listNextStates = []
+        self.listVisitedStates = []
+        self.pathToTarget = []
+        self.currentStateW = self.chess.boardSim.currentStateW;
+        self.depthMax = 8;
+        self.checkMate = False
+
+
+    def getCurrentState(self):
+    
+        return self.myCurrentStateW
+    
+    
+    def getListNextStatesW(self, myState):
+    
+        self.chess.boardSim.getListNextStatesW(myState)
+        self.listNextStates = self.chess.boardSim.listNextStates.copy()
+
+        return self.listNextStates
+
+
+    def isSameState(self, a, b):
+        
+        isSameState1 = True
+        # a and b are lists
+        for k in range(len(a)):
+            
+            if a[k] not in b:
+                isSameState1 = False
+                
+        isSameState2 = True
+        # a and b are lists
+        for k in range(len(b)):
+            
+            if b[k] not in a:
+                isSameState2 = False
+        
+        isSameState = isSameState1 and isSameState2
+        return isSameState
+    
+    
+    def isVisited(self, mystate):
+    
+        if (len(self.listVisitedStates)>0):
+            perm_state = list(permutations(mystate))
+            
+            isVisited = False
+            for j in range(len(perm_state)):
+    
+                for k in range(len(self.listVisitedStates)):
+                    
+                    if self.isSameState(list(perm_state[j]), self.listVisitedStates[k]):
+                        
+                        isVisited = True
+            
+            return isVisited
+        else:
+            return False
+
+
+    def isCheckMate(self, mystate):
+        
+            return False
+
+    def DepthFirstSearch(self, currentState, depth):
+        """
+        Check mate from currentStateW
+        """
+        
+        # your code here
+        
+        
+
+    def BreadthFirstSearch(self, currentState, depth):
+        """
+        Check mate from currentStateW
+        """
+        
+        # your code
+            
+
+def translate(s):
+    """
+    Translates traditional board coordinates of chess into list indices
+    """
+
+    try:
+        row = int(s[0])
+        col = s[1]
+        if row < 1 or row > 8:
+            print(s[0] + "is not in the range from 1 - 8")
+            return None
+        if col < 'a' or col > 'h':
+            print(s[1] + "is not in the range from a - h")
+            return None
+        dict = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7}
+        return (8 - row, dict[col])
+    except:
+        print(s + "is not in the format '[number][letter]'")
+        return None
+
+
+if __name__ == "__main__":
+
+ #   if len(sys.argv) < 2:
+ #       sys.exit(usage())
+
+    # intiialize board
+    TA = np.zeros((8, 8))
+    TA[7][0] = 2
+    TA[7][4] = 6
+    TA[0][4] = 12
+
+    # initialise board
+    print("stating AI chess... ")
+    aichess = Aichess(TA, True)
+    currentState = aichess.chess.board.currentStateW.copy()
+
+    print("printing board")
+    aichess.chess.boardSim.print_board()
+
+    # get list of next states for current state
+    print("current State",currentState)
+    
+    # it uses board to get them... careful 
+    aichess.getListNextStatesW(currentState)
+    print("list next states ",aichess.pathToTarget)
+    
+    # starting from current state find the end state (check mate) - recursive function
+    # find the shortest path, initial depth 0
+    depth = 0
+    aichess.DepthFirstSearch(currentState, depth)
+    print("DFS End")
+
+    # example move piece from start to end state
+    MovesToMake = ['1e','2e']
+    print("start: ",MovesToMake[0])
+    print("to: ",MovesToMake[1])
+
+    start = translate(MovesToMake[0])
+    to = translate(MovesToMake[1])
+
+    print("start: ",start)
+    print("to: ",to)
+
+    aichess.chess.moveSim(start, to)
+
+    # aichess.chess.boardSim.print_board()
+    print("#Move sequence...  ",aichess.pathToTarget)
+    print("#Visited sequence...  ",aichess.listVisitedStates)
+
+    print("#Current State...  ", aichess.chess.board.currentStateW)
